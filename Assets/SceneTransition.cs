930cf26e621f1c526a6417840718e5a658a2481a
Victoria Loskutova <viktoriia.loskutova@gmail.com>
2024-11-17 21:25:53 -0500

    transitionbuttons

--- Assets/SceneTransition.cs
+++ Assets/SceneTransition.cs
@@ -1,9 +1,31 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class SceneTransition : MonoBehaviour
 {
     public string targetScene;
+    private TextMeshPro textMesh;
+    private Vector3 originalScale;
+    private float hoverScale = 1.1f;
+
+    void Start()
+    {
+        textMesh = GetComponent<TextMeshPro>();
+        originalScale = transform.localScale;
+    }   
+
+    private void OnMouseEnter()
+    {
+        transform.localScale = originalScale * hoverScale;
+        textMesh.color = Color.yellow;
+    }
+
+    private void OnMouseExit()
+    {
+        transform.localScale = originalScale;
+        textMesh.color = Color.white;
+    }
 
     private void OnMouseDown()
     {
