34a21121568ceb7cbceef1ebe39b4cab25f176e2
Victoria Loskutova <viktoriia.loskutova@gmail.com>
2024-11-22 10:59:16 -0500

    adds files

---
+++ Assets/CatMoodController.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using System.Linq;
+
+public class CatStateController : MonoBehaviour
+{
+    [SerializeField] private HungerSystem hungerSystem;
+    private SpriteRenderer catSprite;
+    private HungerState state;
+
+    [SerializeField] private float goodThreshold = 70f;
+    [SerializeField] private float warningThreshold = 30f;
+
+    [SerializeField] private Color goodColor = Color.green;
+    [SerializeField] private Color warningColor = Color.yellow;
+    [SerializeField] private Color badColor = Color.red;
+
+    void Start()
+    {
+        GameObject catIcon = GameObject.FindWithTag("cat_icon");
+        if (catIcon != null)
+        {
+            catSprite = catIcon.GetComponent<SpriteRenderer>();
+        }
+        else
+        {
+            Debug.LogError("No object with tag 'cat_icon' found!");
+        }
+
+        if (hungerSystem != null)
+        {
+            state = hungerSystem.GetCurrentState();
+        }
+    }
+
+    void Update()
+    {
+        if (hungerSystem != null)
+        {
+            state = hungerSystem.GetCurrentState();
+            UpdateCatColor();
+        }
+    }
+
+    private void UpdateCatColor()
+    {
+        if (catSprite == null || state == null) return;
+
+        int minState = new[] { state.Saturation, state.Pets, state.Plays }.Min();
+
+        Color newColor;
+        if (minState >= goodThreshold)
+        {
+            newColor = goodColor;
+        }
+        else if (minState >= warningThreshold)
+        {
+            newColor = warningColor;
+        }
+        else
+        {
+            newColor = badColor;
+        }
+
+        newColor.a = catSprite.color.a;
+        catSprite.color = newColor;
+    }
+}
